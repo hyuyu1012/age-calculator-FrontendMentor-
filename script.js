d4d5aef63dc7021052ea34b717a81fc38974b398
hyuyu1012 <hanhyeongu495@gmail.com>
2025-09-01 14:43:53 +0900

    error 로직 구현 완료

--- script.js
+++ script.js
@@ -24,14 +24,36 @@ const yearInput = document.querySelector("#input__year");
 const monthInput = document.querySelector("#input__month");
 const dayInput = document.querySelector("#input__day");
 
-const year__label = document.querySelector();
-const month__label = document.querySelector();
-const day__label = document.querySelector();
+const yearLabel = document.querySelector('#label__year');
+const monthLabel = document.querySelector('#label__month');
+const dayLabel = document.querySelector('#label__day');
 
 const root = document.documentElement;
 const primaryRed = getComputedStyle(root).getPropertyValue('--primary-red').trim();
+const grey200 = getComputedStyle(root).getPropertyValue('--Grey-200').trim();
+const grey500 = getComputedStyle(root).getPropertyValue('--Grey-500').trim();
+
+// label : 500
+// input : 200
+
+function resetInput() {
+  dayError.textContent = "";
+  monthError.textContent = "";
+  yearError.textContent = "";
+
+  yearLabel.style.color = grey500;
+  monthLabel.style.color = grey500;
+  dayLabel.style.color = grey500;      
+
+  yearInput.style.borderColor = grey200;
+  monthInput.style.borderColor = grey200;
+  dayInput.style.borderColor = grey200;
+
+}
+
 
 const updateLiveTime = (birthYear, birthMonth, birthDay) => {
+  resetInput();
   const today = new Date();
   const currentYear = today.getFullYear();
   const currentMonth = today.getMonth() + 1; // JS 월은 0~11
@@ -64,12 +86,6 @@ const updateLiveTime = (birthYear, birthMonth, birthDay) => {
   spanDay.textContent = resultDay;
 } 
 
-const resetAll = () => {
-
-
-}
-
-
 submitBtn.addEventListener("click", () => {
   const birthYear = (yearInput.value);
   const birthMonth = (monthInput.value);
@@ -81,16 +97,19 @@ submitBtn.addEventListener("click", () => {
   if(checkEmptyYear(birthYear)) {
     yearError.textContent = "This field is required";
     yearInput.style.borderColor = primaryRed;
+    yearLabel.style.color = primaryRed;
     emptyFlag = true;
   }
   if(checkEmptyMonth(birthMonth)) {
     monthError.textContent= "This field is required";
     monthInput.style.borderColor = primaryRed;
+    monthLabel.style.color = primaryRed;
     emptyFlag = true;
   }
   if(checkEmptyDay(birthDay)) {
     dayError.textContent= "This field is required";
     dayInput.style.borderColor = primaryRed;
+    dayLabel.style.color = primaryRed;
     emptyFlag = true;
   }
   if(emptyFlag) {
@@ -100,16 +119,19 @@ submitBtn.addEventListener("click", () => {
   if(!isValidYear(Number(birthYear))) {
     yearError.textContent = "Must be a valid Year";
     yearInput.style.borderColor = primaryRed;
+    yearLabel.style.color = primaryRed
     validFlag = true;
   }
   if(!isValidMonth(Number(birthMonth))) {
     monthError.textContent = "Must be a valid Month";
     monthInput.style.borderColor = primaryRed;
+    monthLabel.style.color = primaryRed    
     validFlag = true;
   }
   if(!isValidDay(Number(birthDay))) {
     dayError.textContent = "Must be a valid Day";
     dayInput.style.borderColor = primaryRed;
+    dayLabel.style.color = primaryRed    
     validFlag = true;
   }
   if(validFlag){
@@ -118,11 +140,16 @@ submitBtn.addEventListener("click", () => {
   if(!checkPast(Number(birthYear), Number(birthMonth), Number(birthDay))) {
     yearError.textContent = "Must be in the past";
     yearInput.style.borderColor = primaryRed;
+    yearLabel.style.color = primaryRed    
     return;
   }
   if(!isValidDate(Number(birthYear), Number(birthMonth), Number(birthDay))) {
     dayError.textContent = "Must be a valid date";
     dayInput.style.borderColor = primaryRed;
+    yearLabel.style.color = primaryRed;    
+    monthLabel.style.color = primaryRed;    
+    dayLabel.style.color = primaryRed;    
+    
     return;
   }
   updateLiveTime(Number(birthYear), Number(birthMonth), Number(birthDay));
